ce28b97de1c9e261978e25ae5f026b4e7efb6b8b
DCampos <daniel.campos@gmx.us>
2023-05-26 15:19:58 -0300

    improved advertisements

--- backend/advertisements/urls.py
+++ backend/advertisements/urls.py
@@ -3,12 +3,24 @@ from .views import (
     AdvertisementListAPIView,
     AdvertisementDetailAPIView,
     AdvertisementCreateAPIView,
+    AdvertisementSearchAPIView,
+    AdvertisementUpdateAPIView,
+    AdvertisementDeleteAPIView
 )
 
 app_name = 'advertisements'
 
 urlpatterns = [
-    path('advertisements/', AdvertisementListAPIView.as_view(), name='advertisement-list'),
-    path('advertisements/<int:pk>/', AdvertisementDetailAPIView.as_view(), name='advertisement-detail'),
-    path('advertisements/create/', AdvertisementCreateAPIView.as_view(), name='advertisement-create'),
+    path('advertisements/', AdvertisementListAPIView.as_view(),
+         name='advertisement-list'),
+    path('advertisements/<int:pk>/', AdvertisementDetailAPIView.as_view(),
+         name='advertisement-detail'),
+    path('advertisements/create/', AdvertisementCreateAPIView.as_view(),
+         name='advertisement-create'),
+    path('advertisements/search/', AdvertisementSearchAPIView.as_view(),
+         name='advertisement-search'),
+    path('advertisements/<int:pk>/update/',
+         AdvertisementUpdateAPIView.as_view(), name='advertisement-update'),
+    path('advertisements/<int:pk>/delete/', AdvertisementDeleteAPIView.as_view(),
+         name='advertisement-delete')
 ]
--- backend/advertisements/views.py
+++ backend/advertisements/views.py
@@ -1,7 +1,9 @@
-from rest_framework import generics, serializers
+from rest_framework import generics, serializers, permissions
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
+from django.db.models import Q
 
 
 class AdvertisementCreateAPIView(generics.CreateAPIView):
@@ -27,3 +29,50 @@ class AdvertisementListAPIView(generics.ListAPIView):
 class AdvertisementDetailAPIView(generics.RetrieveAPIView):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
+
+
+class AdvertisementSearchAPIView(generics.ListAPIView):
+    serializer_class = AdvertisementSerializer
+
+    def get_queryset(self):
+        search_query = self.request.query_params.get('search', '')
+        if search_query:
+            queryset = Advertisement.objects.filter(
+                Q(description__icontains=search_query) |
+                Q(category__icontains=search_query) |
+                Q(price__icontains=search_query) |
+                Q(created_at__icontains=search_query)
+            )
+        else:
+            queryset = Advertisement.objects.all()
+        return queryset
+
+
+class AdvertisementUpdateAPIView(generics.UpdateAPIView):
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        # Obtém o anúncio pelo ID e verifica se o usuário atual é o criador do anúncio
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            # Levanta uma exceção se o usuário não for o criador do anúncio
+            raise PermissionDenied(
+                "You do not have permission to edit this advertisement.")
+        return obj
+
+
+class AdvertisementDeleteAPIView(generics.DestroyAPIView):
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        # Obtém o anúncio pelo ID e verifica se o usuário atual é o criador do anúncio
+        obj = super().get_object()
+        if obj.user != self.request.user:
+            # Levanta uma exceção se o usuário não for o criador do anúncio
+            raise PermissionDenied(
+                "You do not have permission to delete this advertisement.")
+        return obj
