f86f2d5c60f10c69a0f9b92182913b2ea0a29daa
DCampos <daniel.campos@gmx.us>
2023-05-25 22:57:54 -0300

    added migrations

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('users/', UserListAPIView.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'),
     path('users/create/', UserListCreateAPIView.as_view(), name='user-list-create'),
-    path('users/<int:pk>/', UserRetrieveUpdateAPIView.as_view(), name='user-retrieve-update'),
+    path('users/<int:pk>/update/', UserRetrieveUpdateAPIView.as_view(), name='user-retrieve-update'),
     path('token/', ObtainTokenPairView.as_view(), name='token-obtain-pair'),
     path('token/refresh/', RefreshTokenView.as_view(), name='token-refresh')
 ]
