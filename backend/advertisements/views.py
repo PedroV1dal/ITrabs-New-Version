d9af946022d95498a9652325ac8a05e622434403
Dcampos <daniel.campos@gmx.us>
2023-06-01 15:04:09 -0300

    added views comments

--- backend/advertisements/views.py
+++ backend/advertisements/views.py
@@ -7,6 +7,9 @@ from django.db.models import Q
 
 
 class AdvertisementCreateAPIView(generics.CreateAPIView):
+    '''
+    Cria novos anúncios.
+    '''
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     permission_classes = [IsAuthenticated]
@@ -22,21 +25,32 @@ class AdvertisementCreateAPIView(generics.CreateAPIView):
 
 
 class AdvertisementListAPIView(generics.ListAPIView):
+    '''
+    Retorna uma lista de todos os anúncios existentes.
+    '''
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
 
 
 class AdvertisementDetailAPIView(generics.RetrieveAPIView):
+    '''
+    Retorna os detalhes de um anúncio específico com base no seu ID.
+    '''
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
 
 
 class AdvertisementSearchAPIView(generics.ListAPIView):
+    '''
+    Pesquisa anúncios com base em um parâmetro de consulta fornecido.
+    '''
     serializer_class = AdvertisementSerializer
 
     def get_queryset(self):
         search_query = self.request.query_params.get('query', '')
         if search_query:
+            # Realizar uma pesquisa usando filtros no campo 'description',
+            # 'category', 'price' e 'created_at'
             queryset = Advertisement.objects.filter(
                 Q(description__icontains=search_query) |
                 Q(category__icontains=search_query) |
@@ -44,11 +58,15 @@ class AdvertisementSearchAPIView(generics.ListAPIView):
                 Q(created_at__icontains=search_query)
             )
         else:
+            # Se nenhum parâmetro de pesquisa for fornecido, retornar todos os anúncios
             queryset = Advertisement.objects.all()
         return queryset
 
 
 class AdvertisementUpdateAPIView(generics.UpdateAPIView):
+    '''
+    Atualiza um anúncio existente.
+    '''
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -64,6 +82,9 @@ class AdvertisementUpdateAPIView(generics.UpdateAPIView):
 
 
 class AdvertisementDeleteAPIView(generics.DestroyAPIView):
+    '''
+    Exclui um anúncio existente. 
+    '''
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
     permission_classes = [permissions.IsAuthenticated]
--- backend/users/views.py
+++ backend/users/views.py
@@ -10,6 +10,9 @@ from .serializers import UserSerializer, serializers
 
 
 class UserInfoView(APIView):
+    '''
+    Fornece informações do usuário autenticado.
+    '''
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -20,18 +23,23 @@ class UserInfoView(APIView):
 
 
 class UserListCreateAPIView(generics.CreateAPIView):
+    '''
+    Cria novos usuários.
+    '''
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
-            # Use a serializer with limited fields for POST requests
             return CreateUserSerializer
         return self.serializer_class
 
 
 class CreateUserSerializer(serializers.ModelSerializer):
+    '''
+    Serializa e desserializa dados relacionados à criação de um novo usuário.
+    '''
     class Meta:
         model = CustomUser
         fields = ['email', 'password']
@@ -45,6 +53,9 @@ class CreateUserSerializer(serializers.ModelSerializer):
 
 
 class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    '''
+    Atualiza as informações de um usuário específico.
+    '''
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
@@ -55,55 +66,76 @@ class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
 
 
 class UserDeleteAPIView(generics.DestroyAPIView):
+    '''
+    Exclui um usuário específico.
+    '''
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
 
-class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-
 
 class UserListAPIView(generics.ListAPIView):
+    '''
+    Lista todos os usuários do sistema.
+    '''
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetailAPIView(generics.RetrieveAPIView):
+    '''
+    Lista os detalhes de um usuário específico.
+    '''
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
 
 
 # Utilização de JWT
 class ObtainTokenPairView(TokenObtainPairView):
+    '''
+    Obtém um par de tokens de acesso e atualização.
+    '''
     serializer_class = TokenObtainPairSerializer
     pass
 
 
 class TokenObtainPairSerializer(TokenObtainPairSerializer):
+    '''
+    Serializa e desserializa os dados necessários para a obtenção de um par de tokens.
+    '''
     username_field = 'email'
 
 
 class RefreshTokenView(TokenRefreshView):
+    '''
+    Atualiza um token de acesso expirado.
+    '''
     pass
 
 
     
 @api_view(['POST'])
 def rate_user(request, pk):
+    '''
+    Lida com o processo de votação de um usuário.
+    '''
     try:
+        # Recupera o usuário pelo ID
         user = CustomUser.objects.get(pk=pk)
     except CustomUser.DoesNotExist:
         return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
 
     if user == request.user:
+        # Retorna erro caso o usuário esteja tentando votar em si mesmo.
         return Response({'error': 'You cannot vote for yourself.'}, status=status.HTTP_400_BAD_REQUEST)
 
     rating = request.data.get('rating')
     if rating is None or not isinstance(rating, (int, float)) or rating < 0 or rating > 5:
+        # Retorna erro caso o número seja inválido
         return Response({'error': 'Invalid rating. Rating should be a number between 0 and 5.'}, status=status.HTTP_400_BAD_REQUEST)
 
+    # Atualiza os campos de classificação do usuário
     user.total_rating += rating
     user.num_ratings += 1
     user.average_rating = user.total_rating / user.num_ratings
